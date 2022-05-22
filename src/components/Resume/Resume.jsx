5b292c24a614429a7a7b7da5355f2e0f25b07b0b
Patricia Edwards <patriciaje16@gmail.com>
2022-05-22 15:34:55 +0100

    added resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -5,7 +5,11 @@ const Resume = () => {
   return (
     <div className='Resume-container'>
       <h1 id="resume" className="Resume-header">Resume</h1>
-      <p className='paragraph2'>yo yo</p>
+      <p className='paragraph2'> <ul>
+        <li>International School of Tanganyika: IB diploma</li>
+        <li>University of Nottingham: Occupational Psychology</li>
+        <li>British Airways Executive Club: Customer Advisor</li>
+      </ul></p>
     </div>
   )
 }
